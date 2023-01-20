5f599eae44b5d5653cdd046c920f0d9ae96e117a
Son Nghiem <nghiemminhson@gmail.com>
2023-01-20 12:38:29 +0100

    Initial commit

---
+++ src/test/java/g45_lexicon/spring_boot_workshop/SpringBootWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package g45_lexicon.spring_boot_workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
