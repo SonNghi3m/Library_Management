27107c9095c43d01ae540e9d6610aae7c709982d
Son Nghiem <nghiemminhson@gmail.com>
2023-01-27 09:35:50 +0100

    part 5 completed.

--- src/main/java/g45_lexicon/spring_boot_workshop/dao/DetailsDaoImpl.java
+++ src/main/java/g45_lexicon/spring_boot_workshop/dao/DetailsDaoImpl.java
@@ -14,38 +14,38 @@ import java.util.Collection;
 @Repository
 public class DetailsDaoImpl implements DetailsDao {
     @PersistenceContext
-    EntityManager entityManager;
+    EntityManager em;
     @Override
     @Transactional(readOnly = true)
     public Details findById(int id) {
-        return entityManager.find(Details.class, id);
+        return em.find(Details.class, id);
     }
 
     @Override
     @Transactional(readOnly = true)
     public Collection<Details> findAll() {
-        return entityManager.createQuery("select d from Details d", Details.class).getResultList();
+        return em.createQuery("select d from Details d", Details.class).getResultList();
     }
 
     @Override
     @Transactional
     public Details create(Details details) {
-        entityManager.persist(details);
+        em.persist(details);
         return details;
     }
 
     @Override
     @Transactional
     public Details update(Details details) {
-        entityManager.merge(details);
+        em.merge(details);
         return details;
     }
 
     @Override
     @Transactional
     public void delete(int id) throws DataNotFoundException{
-        Details details = entityManager.find(Details.class, id);
-        if (details != null) entityManager.remove(details);
+        Details details = em.find(Details.class, id);
+        if (details != null) em.remove(details);
         else throw new DataNotFoundException("details was not found!");
     }
 }
